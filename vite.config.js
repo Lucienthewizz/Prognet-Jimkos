c23f3e7dc2ae874b33ccb04d63a2f9f642644f89
Lucien Purnawarman <lucienthewizz@gmail.com>
2024-10-28 12:39:31 +0800

    Build Dist

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // Add path module
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
             refresh: true,
+            buildDirectory: 'public/build', // Specify where to output built files
         }),
         react(),
     ],
